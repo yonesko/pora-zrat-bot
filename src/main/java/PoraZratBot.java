0bdb74e703f357045a2c45d0deb295fed008ac27
Gleb Danichev <glebio@yandex-team.ru>
2019-08-26 18:18:51 +0300

    prod

--- src/main/java/PoraZratBot.java
+++ src/main/java/PoraZratBot.java
@@ -20,7 +20,7 @@ public class PoraZratBot extends TelegramLongPollingBot {
 
     private final List<String> карательнаяКулинарияStickerIds;
 
-    private final boolean testRun = true;
+    private final boolean testRun = false;
 
     private final long КЛУБ_ЛЮБИТЕЛЕЙ_ПОЕСТЬ_CHAT_ID = testRun ? GLEB_CHAT_ID : -295100024;
 
