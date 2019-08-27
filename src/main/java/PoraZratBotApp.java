37b19e6be94be863f7532a0f689d61f1f8b5ef18
Gleb Danichev <glebio@yandex-team.ru>
2019-08-27 15:00:08 +0300

    PoraZratBotApp

--- src/main/java/PoraZratBotApp.java
+++ src/main/java/PoraZratBotApp.java
@@ -2,7 +2,7 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class Main {
+public class PoraZratBotApp {
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
