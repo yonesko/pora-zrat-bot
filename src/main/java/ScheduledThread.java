b86783abe5abddeffd7df4bac2ef6153e89533f7
Gleb Danichev <glebio@yandex-team.ru>
2019-09-02 12:23:38 +0300

    ScheduledThreadTest

--- src/main/java/ScheduledThread.java
+++ src/main/java/ScheduledThread.java
@@ -1,3 +1,4 @@
+import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.function.Supplier;
@@ -8,21 +9,24 @@ import org.apache.logging.log4j.Logger;
 /**
  * @author Gleb Danichev
  */
-public abstract class ScheduledThread extends Thread {
+public class ScheduledThread extends Thread {
 
     private final static Logger logger = LogManager.getLogger();
 
     private final Supplier<Instant> nextWakeupSupplier;
 
-    public ScheduledThread(Runnable runnable, Supplier<Instant> nextWakeupSupplier) {
+    private final Clock clock;
+
+    public ScheduledThread(Runnable runnable, Supplier<Instant> nextWakeupSupplier, Clock clock) {
         super(runnable);
         this.nextWakeupSupplier = nextWakeupSupplier;
+        this.clock = clock;
     }
 
     @Override
     public final void run() {
         while (true) {
-            Duration between = Duration.between(Instant.now(), nextWakeupSupplier.get());
+            Duration between = Duration.between(Instant.now(clock), nextWakeupSupplier.get());
             if (between.isNegative() || between.isZero()) {
                 super.run();
             } else {
--- src/test/java/ScheduledThreadTest.java
+++ src/test/java/ScheduledThreadTest.java
@@ -1,3 +1,7 @@
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+
 import org.junit.Test;
 
 /**
@@ -6,7 +10,14 @@ import org.junit.Test;
 public class ScheduledThreadTest {
 
     @Test
-    public void test() {
+    public void test() throws InterruptedException {
+        StringBuilder sb = new StringBuilder();
+        ScheduledThread scheduledThread = new ScheduledThread(
+            () -> sb.append(Instant.now().toEpochMilli()),
+            () -> Instant.ofEpochMilli(6000),
+            Clock.fixed(Instant.ofEpochMilli(5000), ZoneId.systemDefault())
+        );
+
 
     }
 }
