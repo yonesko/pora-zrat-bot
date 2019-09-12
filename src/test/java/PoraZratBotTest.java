e9a64e053d5d8fbf2a80199581a7a47420123b8b
Gleb Danichev <glebio@yandex-team.ru>
2019-09-12 18:12:31 +0300

    onUpdateReceivedTest

--- src/test/java/PoraZratBotTest.java
+++ src/test/java/PoraZratBotTest.java
@@ -1,4 +1,8 @@
 import org.junit.Test;
+import org.mockito.internal.util.reflection.FieldSetter;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doCallRealMethod;
@@ -9,9 +13,39 @@ import static org.mockito.Mockito.mock;
  */
 public class PoraZratBotTest {
 
+    private final PoraZratBot bot;
+
+    public PoraZratBotTest() {
+        bot = mock(PoraZratBot.class);
+        doCallRealMethod().when(bot).onUpdateReceived(any());
+    }
+
     @Test
     public void onUpdateReceivedTest() {
-        PoraZratBot bot = mock(PoraZratBot.class);
-        doCallRealMethod().when(bot).onUpdateReceived(any());
+
+
+    }
+
+    private void sendToBot(long chatId, String text) throws NoSuchFieldException {
+        bot.onUpdateReceived(buildUpdate(buildMessage(text, buildChat(chatId))));
+    }
+
+    private Update buildUpdate(Message message) throws NoSuchFieldException {
+        Update update = new Update();
+        FieldSetter.setField(update, Update.class.getDeclaredField("message"), message);
+        return update;
+    }
+
+    private Message buildMessage(String text, Chat chat) throws NoSuchFieldException {
+        Message message = new Message();
+        FieldSetter.setField(message, Message.class.getDeclaredField("text"), text);
+        FieldSetter.setField(message, Message.class.getDeclaredField("chat"), chat);
+        return message;
+    }
+
+    private Chat buildChat(long chatId) throws NoSuchFieldException {
+        Chat chat = new Chat();
+        FieldSetter.setField(chat, Chat.class.getDeclaredField("id"), chatId);
+        return chat;
     }
 }
