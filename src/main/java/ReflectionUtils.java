97d26116998e6c1a432553e16050c7d5cfc75cbe
Gleb Danichev <glebio@yandex-team.ru>
2019-09-12 18:29:28 +0300

    PoraZratBotTest

---
+++ src/main/java/ReflectionUtils.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Field;
+
+
+/**
+ * @author Gleb Danichev
+ */
+public class ReflectionUtils {
+
+    public static Field getDeclaredField(Class clazz, String name) {
+        try {
+            return clazz.getDeclaredField(name);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/PoraZratBotTest.java
+++ src/test/java/PoraZratBotTest.java
@@ -1,51 +1,67 @@
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import org.junit.Test;
 import org.mockito.internal.util.reflection.FieldSetter;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.mock;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 /**
  * @author Gleb Danichev
  */
 public class PoraZratBotTest {
 
-    private final PoraZratBot bot;
-
-    public PoraZratBotTest() {
-        bot = mock(PoraZratBot.class);
-        doCallRealMethod().when(bot).onUpdateReceived(any());
-    }
+    private final DummyPoraZratBot bot = new DummyPoraZratBot();
 
     @Test
     public void onUpdateReceivedTest() {
-
+        sendToBot(19, "Привет");
 
     }
 
-    private void sendToBot(long chatId, String text) throws NoSuchFieldException {
+    private BotApiMethod sendToBot(long chatId, String text) {
         bot.onUpdateReceived(buildUpdate(buildMessage(text, buildChat(chatId))));
+        return bot.responses.peek();
     }
 
-    private Update buildUpdate(Message message) throws NoSuchFieldException {
+    private Update buildUpdate(Message message) {
         Update update = new Update();
-        FieldSetter.setField(update, Update.class.getDeclaredField("message"), message);
+        FieldSetter.setField(update, ReflectionUtils.getDeclaredField(Update.class, "message"), message);
         return update;
     }
 
-    private Message buildMessage(String text, Chat chat) throws NoSuchFieldException {
+    private Message buildMessage(String text, Chat chat) {
         Message message = new Message();
-        FieldSetter.setField(message, Message.class.getDeclaredField("text"), text);
-        FieldSetter.setField(message, Message.class.getDeclaredField("chat"), chat);
+        FieldSetter.setField(message, ReflectionUtils.getDeclaredField(Message.class, "text"), text);
+        FieldSetter.setField(message, ReflectionUtils.getDeclaredField(Message.class, "chat"), chat);
         return message;
     }
 
-    private Chat buildChat(long chatId) throws NoSuchFieldException {
+    private Chat buildChat(long chatId) {
         Chat chat = new Chat();
-        FieldSetter.setField(chat, Chat.class.getDeclaredField("id"), chatId);
+        FieldSetter.setField(chat, ReflectionUtils.getDeclaredField(Chat.class, "id"), chatId);
         return chat;
     }
+
+    private static class DummyPoraZratBot extends PoraZratBot {
+
+        private final Queue<BotApiMethod> responses = new LinkedList<>();
+
+        DummyPoraZratBot() {
+        }
+
+        @Override
+        public <T extends Serializable, Method extends BotApiMethod<T>> T execute(Method method) throws
+            TelegramApiException
+        {
+            responses.add(method);
+            return super.execute(method);
+        }
+
+    }
+
 }
